0cfcfb159cb62a0595f480e64b55d0fead3cbadc
liangchanglu <liangcl523@163.com>
2020-04-27 10:29:45 +0800

    切换搜索源

---
+++ src/pages/navigator/components/shiftItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./shiftItem.scss";
+function App() {
+  return (
+    <div className="shiftItem">
+      <p>知乎</p>
+      <p>掘金</p>
+      <p>思否</p>
+      <p></p>
+    </div>
+  );
+}
+export default App;
--- src/pages/navigator/index.js
+++ src/pages/navigator/index.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import "../../style/nav1.scss";
+import PickItem from "./components/shiftItem.js";
 export default function App() {
   let initInputValue = "";
   const [inputValue, setInputValue] = useState(initInputValue);
-  function getInputValue(){
-
+  function getInputValue(event) {
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  }
+  function search(params) {
+    // 知乎
+    window.location.href = `https://www.zhihu.com/search?type=content&q=${inputValue}`;
+    // 思否
+    window.location.href = `https://segmentfault.com/search?q=${inputValue}`;
+    // 掘金
+    window.location.href = `https://juejin.im/search?query=${inputValue}&type=all`;
   }
   return (
     <div className="nav1">
@@ -13,6 +23,7 @@ export default function App() {
         <header className="header">
           <p>前端导航</p>
         </header>
+        <PickItem />
         <div className="input-wrap">
           <div className="input-content">
             <input
@@ -20,7 +31,9 @@ export default function App() {
               onChange={getInputValue}
               placeholder="输入关键字"
             />
-            <span className="nav-search">搜索</span>
+            <span className="nav-search" onClick={search}>
+              搜索
+            </span>
           </div>
         </div>
       </div>
